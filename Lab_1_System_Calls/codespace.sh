0e967b270a855ee507bdf04dcda78dec70e75c26
anj501 <110501128+anj501@users.noreply.github.com>
2024-06-24 15:48:23 -0700

    Delete Lab_1_System_Calls directory

--- Lab_1_System_Calls/codespace.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get update && sudo apt-get install -y qemu-system-i386
-echo "set auto-load safe-path /" > ~/.gdbinit
-code --install-extension mingjun97.cs153-debugging-pack
--- Lab_1_System_Calls/test.c
+++
@@ -1,9 +0,0 @@
-#include "types.h"
-#include "stat.h"
-#include "user.h"
-
-int main(int argd, char*argv[]) {
-    //printf(1, "hello world\n");
-    hello(); //J.H.
-}
-
